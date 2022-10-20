d92ee79e55f519736768a3b9c5b416cdb16dbd04
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-20 12:11:33 +0300

    category and news api created by id

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 var cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -11,10 +12,26 @@ app.get('/', (req, res) => {
     res.send('News api running')
 });
 
-
-app.get('/catagories', (req, res) => {
+app.get('/news-categories', (req, res) => {
     res.send(categories)
 });
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news)
+    }else{
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+    
+})
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
 
 
 app.listen(port , ()=>{
